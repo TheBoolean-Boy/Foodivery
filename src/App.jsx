b6414cdf571a5d1f959fb6d7c8440b20248c7a90
TheBoolean-Boy <saswatrath02@gmail.com>
2025-08-13 22:10:33 +0530

    Login routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,20 @@ import Home from "./Pages/Home/Home";
 import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder";
 import Cart from "./Pages/Cart/Cart";
 import Footer from "./Components/Footer/Footer";
+import { useState } from "react";
+import LoginPopup from "./Components/LoginPopup/LoginPopup";
 
 
 function App() {
+  const [showLogin, setShowLogin] = useState(false);
+
   return (
     <>
-
+    {
+      showLogin?<LoginPopup  setShowLogin={setShowLogin}/>: <></>
+    }
       <div className="app">
-        <Navbar />
+        <Navbar  setShowLogin={setShowLogin}/>
 
         <Routes>
           <Route path="/" element={<Home />} />
