23a0af01d4167d0f02c52d4b43e20cf00d5e1ffa
TheBoolean-Boy <saswatrath02@gmail.com>
2025-08-13 16:39:32 +0530

    Context API setup

---
+++ src/Components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,23 @@
+import { useContext } from 'react'
+import './FoodDisplay.jsx'
+import {StoreContext} from '../../Context/StoreContext'
+import FoodItem from '../FoodItem/FoodItem.jsx'
+
+const FoodDisplay = ({category}) => {
+
+  const {food_list} = useContext(StoreContext)
+  return(
+    <div className='food-display' id='food-display'>
+    <h2>Top dishes near you</h2>
+    <div className="food-display-list">
+      {food_list.map( (item, index) => {
+        return(
+          <FoodItem />
+        )
+      })}
+    </div>
+    </div>
+  )
+}
+
+export default FoodDisplay
---
+++ src/Components/FoodItem/FoodItem.jsx
@@ -0,0 +1,11 @@
+import './FoodItem.css'
+
+function FoodItem({id, name, price, description, image}){
+  return(
+    <div>
+
+    </div>
+  )
+}
+
+export default FoodItem;
